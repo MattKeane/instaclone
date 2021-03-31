51af038f84b1cc4032a77f47cba024d185be82eb
Matt Keane <mattpkeane@gmail.com>
2021-03-31 12:47:04 -0500

    create photo route uploads photos

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -1,5 +1,9 @@
 const express = require("express")
 const router = express.Router()
+const multer = require("multer")
+const storage = multer.memoryStorage()
+const upload = multer({ storage })
+const Photo = require("../models/photo")
 
 // Middleware
 router.use((req, res, next) => {
@@ -7,20 +11,46 @@ router.use((req, res, next) => {
 	next()
 })
 
-// Photo homepage
-router.get("/", (req, res) => {
-	if (req.session.user) {
-		res.render("photos/home.ejs")
-	} else {
-		req.message = "You must be signed in to add a photo"
+const requireLogin = (req, res, next) => {
+	if (!req.session.user) {
+		req.session.message = "You must be logged in to do that"
 		res.redirect("/auth/login")
+	} else {
+		next()
 	}
-	
+}
+
+// Photo homepage
+router.get("/", requireLogin, (req, res) => {
+	res.render("photos/home.ejs")
 })
 
 // New photo page
-router.get("/new", (req, res) => {
+router.get("/new", requireLogin, (req, res) => {
 	res.render("photos/new.ejs")
+})
+
+// Create photo route
+router.post("/new", upload.single("image"), async (req, res, next) => {
+	try {
+		const createdPhoto = await Photo.create({
+			caption: req.body.caption,
+			author: req.session.user._id,
+			image: {
+				data: req.file.buffer,
+				contentType: req.file.mimetype,
+			},
+		})
+		const d = new Date()
+		console.log(`${d.toLocaleString}: New photo created:`)
+		console.log(createdPhoto)
+		res.redirect("/photos")
+	} catch (err) {
+		const d = new Date()
+		console.error(`${d.toLocaleString()}: Error creating new photo:`)
+		console.error(err)
+		next(err)
+	}
 })
 
 module.exports = router
